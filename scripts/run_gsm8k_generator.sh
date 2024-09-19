c16f5b206d9ed4cf60fad9a7dc6e13b21b42cf96
jiahangxu <jiahangxu@microsoft.com>
2024-09-18 22:55:12 -0700

    update arguments

--- scripts/run_gsm8k_generator.sh
+++ scripts/run_gsm8k_generator.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python run_src/do_generate.py \
-    --dataset_name GSM8K \
+    --dataset_name AMC \
     --test_json_filename test_all \
-    --model_ckpt mistralai/Mistral-7B-v0.1 \
+    --model_ckpt ../Mistral-7B-v0.1 \
     --note default \
     --num_rollouts 16
