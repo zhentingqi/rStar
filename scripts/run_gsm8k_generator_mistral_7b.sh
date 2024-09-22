6642e3f66aa94a3a1dae7fa587eb1d9cc2edda76
jiahangxu <jiahangxu@microsoft.com>
2024-09-21 18:15:31 -0700

    update ssh

--- scripts/run_gsm8k_generator_mistral_7b.sh
+++ scripts/run_gsm8k_generator_mistral_7b.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=3 python run_src/do_generate.py \
     --dataset_name AMC \
     --test_json_filename test_all \
     --model_ckpt ../Mistral-7B-v0.1 \
-    --note default \
+    --note v1_v0_v0 \
     --num_rollouts 32 \
     --run_outputs_root /mnt/teamdrive/jiahang/rStar/ \
     --eval_outputs_root /mnt/teamdrive/jiahang/rStar/
--- scripts/run_gsm8k_generator_phi3_4k_instruct.sh
+++ scripts/run_gsm8k_generator_phi3_4k_instruct.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=2 python run_src/do_generate.py \
     --dataset_name AMC \
     --test_json_filename test_all \
     --model_ckpt ../Phi-3-mini-4k-instruct_folio \
-    --note default \
+    --note v1_v0_v0 \
     --num_rollouts 32 \
     --run_outputs_root /mnt/teamdrive/jiahang/rStar/ \
     --eval_outputs_root /mnt/teamdrive/jiahang/rStar/
