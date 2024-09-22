9c2b83ed8c32915a70c16cd2083ebb6f991f7fee
jiahangxu <jiahangxu@microsoft.com>
2024-09-21 18:22:33 -0700

    add AIME dataset

---
+++ scripts/run_aime_generator_phi3_4k_instruct.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=2 python run_src/do_generate.py \
+    --dataset_name AIME \
+    --test_json_filename test_all \
+    --model_ckpt ../Phi-3-mini-4k-instruct_folio \
+    --note v0_v0_v0 \
+    --num_rollouts 32 \
+    --run_outputs_root /mnt/teamdrive/jiahang/rStar/ \
+    --eval_outputs_root /mnt/teamdrive/jiahang/rStar/
