98c6c16108b59382d9166178a291844d1950cd0d
Mingyuan Ma <minma@boslogin08.rc.fas.harvard.edu>
2024-11-15 15:31:03 -0500

    update

--- scripts/run_gsm8k_generator.sh
+++ scripts/run_gsm8k_generator.sh
@@ -1,6 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 python run_src/do_generate.py \
-    --dataset_name GSM8K \
+    --dataset_name AMC2024 \
     --test_json_filename test_all \
     --model_ckpt microsoft/Phi-3-mini-4k-instruct \
-    --note default \
-    --num_rollouts 16
+    --note amc \
+    --num_rollouts 16 \
+    --verbose  
